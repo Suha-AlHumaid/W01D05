a3b8696bfeb1280a064faf620bd27bba61c91864
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-10-14 11:34:27 +0300

    First Task

---
+++ myVariables.js
@@ -0,0 +1,15 @@
+// FirstTask
+let language = "JavaScript";
+let score = 10;
+ console.log(language);
+ console.log(score);
+
+ let length = 10;
+ let width = 15;
+area = length*width;
+console.log(`The area of the rectangle is ${area}`);
+
+let num1 =10
+let num2 =20
+console.log(num1%num2);
+
