dfe8295728fe1586a008b580d8c17dde67faf02a
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-10-14 12:29:39 +0300

    Task two

---
+++ dataTypes.js
@@ -0,0 +1,23 @@
+let integer= 0;
+let string ="hello from the other world";
+let array =["HTML", "CSS","JS"];
+let object = {
+    name: 'Suha',
+    age: 30,
+    job: `Programmer`
+};
+console.log(integer);
+console.log(string);
+console.log(array);
+console.log(object);
+
+console.log(typeof integer); 
+console.log(typeof string);
+console.log(typeof array);
+console.log(typeof object);
+let arrayWithNumAndString = ['Hello','Tuwiq',1000];
+// I expect to be string araay datatype
+console.log(typeof arrayWithNumAndString);
+
+var x=null;
+var y ;
